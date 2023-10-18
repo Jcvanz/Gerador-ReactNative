f0c881c894b77fa9d95b54ffdb004a8ad74c8911
Julio <j.vanz1108@gmail.com>
2023-10-17 23:12:21 -0300

    Gerador de Senhas React Native

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home } from "./pages/home";
+import { Passwords } from "./pages/passwords";
+import { Ionicons } from '@expo/vector-icons';
+
+// Configuração das Rotas
+const Tab = createBottomTabNavigator();
+
+export function Routes() {
+    return(
+        <Tab.Navigator>
+
+            {/* ICONE DE HOME DO MENU */}
+            <Tab.Screen 
+                name="Home" 
+                component={Home} 
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused) {
+                            return <Ionicons size={size} color={color} name="home"/>
+                        }
+                        return <Ionicons size={size} color={color} name="home-outline"/>
+                    }
+                }}
+            />
+
+            {/* ICONE DE SENHA DO MENU */}
+            <Tab.Screen 
+                name="Passwords" 
+                component={Passwords} 
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused) {
+                            return <Ionicons size={size} color={color} name="lock-closed"/>
+                        }
+                        return <Ionicons size={size} color={color} name="lock-closed-outline"/>
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
